000344d783b5a09625a2a5d2fcd1d3388e259347
Sergei <serge-mur@yandex.ru>
2019-05-15 11:19:43 +0300

    add vuejs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-$(document).ready(function () {
-    $('.sidenav').sidenav();
+// $(document).ready(function () {
+//     $('.sidenav').sidenav();
+// });
+
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, { edge: "left" });
 });
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: ''
+    }
+})
